1da165478fb34fc2b707297244c9c5818c038b8a
nirpal rawat <nirpalrawat@gmail.com>
2024-12-16 23:53:22 +0530

    hg

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,21 +1,18 @@
-const mongoose =require('mongoose')
+const mongoose = require ('mongoose')
+const Local_url = 'mongodb://127.0.0.1:27017/blogprojact';
+const live_url = 'mongodb+srv://nirpalrawat:LHQXtw6MVi6suedl@cluster0.1simf.mongodb.net/blogportal?retryWrites=true&w=majority&appName=Cluster0'
+
+const connectDB =()=>{
+    return mongoose.connect(live_url)
+
+    .then(()=>{
+        console.log("mongoose connected succefully")
 
 
-const connectDb = () =>{
+    })
+    .catch((error)=>{
+        console.log(error)
 
-    return mongoose.connect('mongodb://127.0.0.1:27017/blogprojject')
-
-.then(()=>{
-
-    console.log(' nirpal connect Db')
-
-})
-.catch((error)=>{
-
-    console.log(error)
-})
-
-
+    })
 }
-
-module.exports = connectDb
+module.exports = connectDB
